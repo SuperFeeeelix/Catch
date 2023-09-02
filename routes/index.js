8197ae5e922491aa112aea982aa62bdc3a114ea5
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-09-01 22:32:57 -0700

    added code

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./users-routes');
 const thoughtRoutes = require('./thought-routes');
-const reactionRoutes = require('./Reaction-routes');
+const reactionRoutes = require('./reaction-routes');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
