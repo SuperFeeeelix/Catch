fa51cc67f599ab5b77561e90c7fa81522089e217
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-09-14 18:33:09 -0700

    added files

---
+++ controllers/thought.js
@@ -0,0 +1,13 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find({});
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('/api', routes);
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log('Connected to MongoDB');
     
 }).catch(error => {
